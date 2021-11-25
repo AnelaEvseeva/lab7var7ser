3fe9f7af552fc33c0b34c63d5f7a09ae70122bdd
AnelaEvseeva <s.flake02@mail.ru>
2021-11-25 19:30:20 +0900

    Initial commit

---
+++ src/com/company/People.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.io.Serializable;
+
+public class People implements Serializable{
+    String lastname;
+    String name;
+    String sex;
+    int height;
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "lastname='" + lastname + '\'' +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", height=" + height +
+                '}';
+    }
+}
